f25759aff3d93e9e030766f5180ad91fd3b869a0
Talha Karasu <talhafb1223@gmail.com>
2020-07-08 18:20:27 +0300

    second menu's color changed

--- dropdown.js
+++ dropdown.js
@@ -8,8 +8,8 @@ export const dropdown = function (btn, menu) {
         menu.style.display = "none"
       }
     } else if (
-      e.target.className !=
-      document.querySelectorAll(`.${menu.className} > li`)[0].className
+      e.target.classList[0] !=
+      document.querySelectorAll(`.${menu.className} > li`)[0].classList[0]
     ) {
       menu.style.display = "none"
     }
