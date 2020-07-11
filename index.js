322a4dedca79baea36f7c10ac57976382af86889
Talha Karasu <talhafb1223@gmail.com>
2020-07-11 20:13:06 +0300

    deprecated codes deleted

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 import { dropdown } from "./dropdown.js"
 
-/* const toggleBtnUl = document.querySelectorAll(".main-list-item")
-const toggleListUl = document.querySelectorAll(".toggle-list") */
-const toggleBtnBtn = document.querySelector(".toggle-btn")
-const toggleListDiv = document.querySelector(".dropdown-menu")
-const toggleBtnA = document.querySelector(".toggle-a")
-const toggleListOl = document.querySelector(".dropdown-orderedlist")
-
-/* dropdown(toggleBtnUl[0], toggleListUl[0])
-dropdown(toggleBtnUl[1], toggleListUl[1]) 
-dropdown(toggleBtnBtn, toggleListDiv)
-dropdown(toggleBtnA, toggleListOl) */
-
 dropdown()
