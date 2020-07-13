2cc7f6e4065aa534a28bceaf2bbf6a0a630fe96a
Talha Karasu <talhafb1223@gmail.com>
2020-07-13 19:02:41 +0300

    deprecated codes deleted

--- dropdown.js
+++ dropdown.js
@@ -58,36 +58,3 @@ export const dropdown = function (btn, menu) {
     }
   })
 }
-
-/* menu.style.display = "none"
-if (type == "click") {
-  document.addEventListener("click", function (e) {
-    if (e.target == btn) {
-      if (menu.style.display != "block") {
-        menu.style.display = "block"
-      } else {
-        menu.style.display = "none"
-      }
-    } else if (
-      e.target.classList[0] !=
-      document.querySelectorAll(`.${menu.classList[0]} > *`)[0].classList[0]
-    ) {
-      menu.style.display = "none"
-    }
-  })
-} else if (type == "hover") {
-  if (!btn.getAttribute("tabindex")) btn.setAttribute("tabindex", 0)
-
-  btn.addEventListener("focus", function () {
-    menu.style.display = "block"
-  })
-  btn.addEventListener("blur", function () {
-    menu.style.display = "none"
-  })
-  btn.addEventListener("mouseover", function () {
-    menu.style.display = "block"
-  })
-  btn.addEventListener("mouseleave", function () {
-    menu.style.display = "none"
-  })
-} */
