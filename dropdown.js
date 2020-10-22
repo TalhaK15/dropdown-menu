bed9ca3114f7903759bb62016361bcd394a4d62b
Talha Karasu <talhafb1223@gmail.com>
2020-10-23 00:01:18 +0300

    modal mode is working well

--- dropdown.js
+++ dropdown.js
@@ -9,69 +9,101 @@ function isElementInViewport(e) {
   )
 }
 
-export const dropdown = function (btn, menu) {
-  const toggleButtons = document.querySelectorAll("[data-toggle=dropdown]")
+export const dropdown = function () {
+  const toggleButtons = document.querySelectorAll("[data-toggle]")
 
   toggleButtons.forEach((btn) => {
-    let menu = document.querySelector(`#${btn.id} + .dropdown`)
-    let type = btn.getAttribute("data-action") || "click"
-    let position = btn.getAttribute("data-position") || "bottom-mid"
-    position = position.split("-")
-    btn.parentNode.style.position = "relative"
-    menu.style.position = "absolute"
-    let menuHeight = menu.offsetHeight
-    let menuWidth = menu.offsetWidth
+    let menuClass = btn.dataset.toggle
+    let menu = document.querySelector(`#${btn.id} + .${menuClass}`)
+    let type = btn.dataset.action || "click"
+    let modal = btn.dataset.modal || "true"
 
-    let setPosition = () => {
-      if (position[0] == "bottom") {
-        menu.style.top = `${btn.offsetHeight}px`
-      } else if (position[0] == "top") {
-        menu.style.top = `-${menuHeight}px`
+    let isSmallScreen = window.matchMedia("(max-width: 700px)").matches
+
+    if (modal == "true" && isSmallScreen) {
+      var modalContainer = document.createElement("div")
+      modalContainer.classList.add("modal-container")
+      menu.classList.add("modal")
+      modalContainer.appendChild(menu)
+      btn.parentNode.insertBefore(modalContainer, btn.nextElementSibling)
+      type = "click"
+    } else {
+      var position = btn.dataset.position || "bottom-mid"
+      position = position.split("-")
+      btn.parentNode.style.position = "relative"
+      menu.style.position = "absolute"
+      var menuHeight = menu.offsetHeight
+      var menuWidth = menu.offsetWidth
+
+      var setPosition = () => {
+        if (position[0] == "bottom") {
+          menu.style.top = `${btn.offsetHeight}px`
+        } else if (position[0] == "top") {
+          menu.style.top = `-${menuHeight}px`
+        }
       }
+
+      setPosition()
+      if (position[1] == "right") {
+        menu.style.left = `${btn.offsetWidth - menuWidth}px`
+      } else if (position[1] == "left") {
+        menu.style.left = 0
+      } else if (position[1] == "mid") {
+        menu.style.left = `${(btn.offsetWidth - menuWidth) / 2}px`
+      }
+
+      menu.style.visibility = "hidden"
+      menu.style.opacity = 0
+      var zindex = menu.style.zIndex
     }
 
-    setPosition()
-
-    if (position[1] == "right") {
-      menu.style.left = `${btn.offsetWidth - menuWidth}px`
-    } else if (position[1] == "left") {
-      menu.style.left = 0
-    } else if (position[1] == "mid") {
-      menu.style.left = `${(btn.offsetWidth - menuWidth) / 2}px`
-    }
-
-    menu.style.visibility = "hidden"
-    menu.style.opacity = 0
-    let zindex = menu.style.zIndex
     if (type == "click") {
       document.addEventListener("click", function (e) {
         if (e.target == btn) {
-          if (menu.style.opacity != 0) {
-            menu.style.zIndex = zindex
-            menu.style.visibility = "hidden"
-            menu.style.opacity = 0
-            setPosition()
-          } else {
-            if (!isElementInViewport(menu)) {
-              if (position[0] == "bottom") {
-                menu.style.top = `-${menuHeight}px`
-              } else if (position[0] == "top") {
-                menu.style.top = `${btn.offsetHeight}px`
-              }
+          if (modal == "true" && isSmallScreen) {
+            if (modalContainer.style.display == "block") {
+              modalContainer.style.display = "none"
+            } else {
+              modalContainer.style.display = "block"
             }
+          } else {
+            if (menu.style.opacity == 1) {
+              menu.style.opacity = 0
+              menu.style.visibility = "hidden"
+              menu.style.zIndex = zindex
 
-            menu.style.zIndex = 999
-            menu.style.visibility = "visible"
-            menu.style.opacity = 1
+              setPosition()
+            } else {
+              if (!isElementInViewport(menu)) {
+                if (position[0] == "bottom") {
+                  menu.style.top = `-${menuHeight}px`
+                } else if (position[0] == "top") {
+                  menu.style.top = `${btn.offsetHeight}px`
+                }
+              }
+
+              menu.style.zIndex = 999
+              menu.style.visibility = "visible"
+              menu.style.opacity = 1
+            }
           }
         } else if (
           e.target.classList[0] !=
           document.querySelectorAll(`.${menu.classList[0]} > *`)[0].classList[0]
         ) {
-          menu.style.zIndex = zindex
-          menu.style.visibility = "hidden"
-          menu.style.opacity = 0
-          setPosition()
+          if (modal == "true" && isSmallScreen) {
+            if (modalContainer.style.display == "block") {
+              modalContainer.style.display = "none"
+            }
+          } else {
+            if (menu.style.opacity == 1) {
+              menu.style.opacity = 0
+              menu.style.visibility = "hidden"
+              menu.style.zIndex = zindex
+
+              setPosition()
+            }
+          }
         }
       })
     } else if (type == "hover") {
@@ -89,9 +121,10 @@ export const dropdown = function (btn, menu) {
         menu.style.opacity = 1
       })
       btn.parentNode.addEventListener("mouseleave", function () {
-        menu.style.zIndex = zindex
-        menu.style.visibility = "hidden"
         menu.style.opacity = 0
+        menu.style.visibility = "hidden"
+        menu.style.zIndex = zindex
+
         setPosition()
       })
     }
